63ea2d1c4b7e3d6d2d6790ec71cd62882d752c5d
bloodjake <mikl90.12.2000@gmail.com>
2022-01-24 19:42:47 +0300

    TestCommit

--- managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
+++ managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001643036461';
-$dateexpire = '001643040061';
+$datecreate = '001643040121';
+$dateexpire = '001643043721';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";b:1;}';
 return true;
 ?>
--- managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
+++ managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001643038141';
-$dateexpire = '001643041801';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1643038142;}';
+$datecreate = '001643040241';
+$dateexpire = '001643043901';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1643040242;}';
 return true;
 ?>
