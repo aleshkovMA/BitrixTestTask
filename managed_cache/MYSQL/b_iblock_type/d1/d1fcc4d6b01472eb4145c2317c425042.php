a1d732ccb9807bfa7c4df0dd5b1b41ee3c1765d0
bloodjake <mikl90.12.2000@gmail.com>
2022-01-27 08:23:52 +0300

    Add infoblock type and infoblock

--- managed_cache/MYSQL/b_iblock_type/d1/d1fcc4d6b01472eb4145c2317c425042.php
+++ managed_cache/MYSQL/b_iblock_type/d1/d1fcc4d6b01472eb4145c2317c425042.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001643260596';
-$dateexpire = '001643296596';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:2:{i:0;a:6:{s:2:"ID";s:7:"banners";s:8:"SECTIONS";s:1:"Y";s:16:"EDIT_FILE_BEFORE";s:0:"";s:15:"EDIT_FILE_AFTER";s:0:"";s:6:"IN_RSS";s:1:"N";s:4:"SORT";s:3:"500";}i:1;a:6:{s:2:"ID";s:9:"documents";s:8:"SECTIONS";s:1:"Y";s:16:"EDIT_FILE_BEFORE";s:0:"";s:15:"EDIT_FILE_AFTER";s:0:"";s:6:"IN_RSS";s:1:"N";s:4:"SORT";s:3:"500";}}}';
+$datecreate = '001643260925';
+$dateexpire = '001643296925';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:3:{i:0;a:6:{s:2:"ID";s:7:"banners";s:8:"SECTIONS";s:1:"Y";s:16:"EDIT_FILE_BEFORE";s:0:"";s:15:"EDIT_FILE_AFTER";s:0:"";s:6:"IN_RSS";s:1:"N";s:4:"SORT";s:3:"500";}i:1;a:6:{s:2:"ID";s:7:"novosti";s:8:"SECTIONS";s:1:"Y";s:16:"EDIT_FILE_BEFORE";s:0:"";s:15:"EDIT_FILE_AFTER";s:0:"";s:6:"IN_RSS";s:1:"N";s:4:"SORT";s:3:"500";}i:2;a:6:{s:2:"ID";s:9:"documents";s:8:"SECTIONS";s:1:"Y";s:16:"EDIT_FILE_BEFORE";s:0:"";s:15:"EDIT_FILE_AFTER";s:0:"";s:6:"IN_RSS";s:1:"N";s:4:"SORT";s:3:"500";}}}';
 return true;
 ?>
--- managed_cache/MYSQL/b_user_field/2c/2c607e2766df4d44c1ecf2c939ae9dc8.php
+++ managed_cache/MYSQL/b_user_field/2c/2c607e2766df4d44c1ecf2c939ae9dc8.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001643183311';
-$dateexpire = '001643186911';
+$datecreate = '001643260980';
+$dateexpire = '001643264580';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:0:{}}';
 return true;
 ?>
